e0732fc837a329a02663bccece476adb31adc05c
Matheus Rodrigues Silveira de Freitas <matheus_rf1997@hotmail.com>
2024-01-15 17:16:28 -0300

    Refatoração para livewire

---
+++ resources/views/livewire/itemtarefa.blade.php
@@ -0,0 +1,37 @@
+<tr class="bg-white dark:bg-gray-800">
+    <td scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+        {{ $tarefa->tarefa }}
+    </td>
+    <td class="px-6 py-4">
+        @if ($tarefa->status === 0)
+
+            <svg class="w-5 h-5 text-red-800 dark:text-red-600 inline-block" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M15.133 10.632v-1.8a5.406 5.406 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V1.1a1 1 0 0 0-2 0v2.364a.946.946 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C4.867 13.018 3 13.614 3 14.807 3 15.4 3 16 3.538 16h12.924C17 16 17 15.4 17 14.807c0-1.193-1.867-1.789-1.867-4.175ZM4 4a1 1 0 0 1-.707-.293l-1-1a1 1 0 0 1 1.414-1.414l1 1A1 1 0 0 1 4 4ZM2 8H1a1 1 0 0 1 0-2h1a1 1 0 1 1 0 2Zm14-4a1 1 0 0 1-.707-1.707l1-1a1 1 0 1 1 1.414 1.414l-1 1A1 1 0 0 1 16 4Zm3 4h-1a1 1 0 1 1 0-2h1a1 1 0 1 1 0 2ZM6.823 17a3.453 3.453 0 0 0 6.354 0H6.823Z"></path>
+            </svg>
+            <div class="inline-block text-red-800 dark:text-red-600">Pendente</div>
+
+            @else
+
+                <svg class="w-6 h-6 text-green-800 dark:text-green-600 inline-block" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path fill="currentColor" d="m18.774 8.245-.892-.893a1.5 1.5 0 0 1-.437-1.052V5.036a2.484 2.484 0 0 0-2.48-2.48H13.7a1.5 1.5 0 0 1-1.052-.438l-.893-.892a2.484 2.484 0 0 0-3.51 0l-.893.892a1.5 1.5 0 0 1-1.052.437H5.036a2.484 2.484 0 0 0-2.48 2.481V6.3a1.5 1.5 0 0 1-.438 1.052l-.892.893a2.484 2.484 0 0 0 0 3.51l.892.893a1.5 1.5 0 0 1 .437 1.052v1.264a2.484 2.484 0 0 0 2.481 2.481H6.3a1.5 1.5 0 0 1 1.052.437l.893.892a2.484 2.484 0 0 0 3.51 0l.893-.892a1.5 1.5 0 0 1 1.052-.437h1.264a2.484 2.484 0 0 0 2.481-2.48V13.7a1.5 1.5 0 0 1 .437-1.052l.892-.893a2.484 2.484 0 0 0 0-3.51Z"></path>
+                    <path class="text-white dark:text-gray-800" d="M8 13a1 1 0 0 1-.707-.293l-2-2a1 1 0 1 1 1.414-1.414l1.42 1.42 5.318-3.545a1 1 0 0 1 1.11 1.664l-6 4A1 1 0 0 1 8 13Z"></path>
+                </svg>
+                <div class="inline-block text-green-800 dark:text-green-600">Finalizada</div>
+            
+        @endif   
+    </td>
+    <td class="px-6 py-4">
+
+        @if ($tarefa->status === 0)
+            <a wire:click="finalizar" class="block text-green-500 hover:underline cursor-pointer">Finalizar</a>
+        @endif
+        @if ($tarefa->status === 1)
+            <a wire:click="estornar" class="block text-red-500 hover:underline cursor-pointer">Estornar</a>
+        @endif
+    
+        <a href="{{ route('tarefa.edit', $tarefa) }}" wire:navigate class="block text-blue-500 hover:underline cursor-pointer">Editar</a>
+        
+        <a wire:click="excluir" wire:click="$refresh" class="block text-red-800 dark:text-red-600 hover:underline cursor-pointer" wire:confirm="Tem certeza que deseja excluir?">Excluir</a>
+        
+    </td>
+</tr>
